c117a911fc2a21456d8d3e1f65f17c98dfc91c38
mhotting <madric.hottinger.101@gmail.com>
2024-06-14 15:17:05 +0200

    Adds new key handlers -> corrects up/down to release/press (files forgotten)

---
+++ srcs/event_handlers/key_release_handler.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_release_handler.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 15:15:55 by mhotting          #+#    #+#             */
+/*   Updated: 2024/06/14 15:16:01 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "cub3d.h"
+#include "event_handlers.h"
+#include "libft.h"
+
+/**
+ * @brief Handles the keyboard release key events
+ * @param data The t_game which contains project data to affect
+ * @param key The integer value of the pressed key
+ * @return A dummy integer
+*/
+int	key_release_handler(int key, t_game *data)
+{
+	if (data == NULL)
+		error_exit(NULL, ERR_ARG);
+	if (key == KEY_ESC)
+		return (destroy_handler(data));
+	ft_printf("Key release: %d\n", key);
+	return (0);
+}
