cb89b0d03a73e67a2ff4d567773c8c52cfaa9006
mhotting <madric.hottinger.101@gmail.com>
2024-06-14 10:48:26 +0200

    Adds forgotten game files

---
+++ srcs/game/t_game_destroy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_game_destroy.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 16:01:50 by mhotting          #+#    #+#             */
+/*   Updated: 2024/06/13 17:01:12 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "cub3d.h"
+#include "mlx_api.h"
+
+/**
+ * @brief Destroys the given t_game
+ * @param game The structure to destroy
+*/
+void	t_game_destroy(t_game *game)
+{
+	if (game == NULL)
+		return ;
+	t_mlx_destroy(&game->mlx);
+}
---
+++ srcs/game/t_game_init.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_game_init.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 16:02:42 by mhotting          #+#    #+#             */
+/*   Updated: 2024/06/13 17:00:05 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+#include "libft.h"
+
+/**
+ * @brief Initializes the given t_game
+ * @param game The structure to init
+ * @return true on SUCCESS, false on ERROR
+*/
+bool	t_game_init(t_game *game)
+{
+	if (game == NULL)
+		return (false);
+	ft_memset(game, 0, sizeof(t_game));
+	return (true);
+}
