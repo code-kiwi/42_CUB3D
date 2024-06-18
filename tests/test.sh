65a312715f47ed9dd7368a2829a9d9482d3b2ff6
Benjamin <benjamin.rappo69@gmail.com>
2024-06-18 09:12:28 +0200

    added a second argument for the use of valgrind

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,13 @@
-if [ $# -eq 0 ]; then
-	echo "no argument supplied"
-	exit ;
+if [ $# -eq 2 ]; then
+	for file in `ls $1/map_error`; do
+		echo $file
+		if [ "$2" = "true" ]; then
+			valgrind ./cub3D "$1/map_error/$file"
+		else
+			./cub3D "$1/map_error/$file"
+		fi
+		read -n1
+	done
+else
+	echo "expected 2 arguments : path to test.sh and true for valgrind"
 fi
-
-for file in `ls $1/map_error`; do
-	echo $file
-	valgrind ./cub3D "$1/map_error/$file"
-	read -n1
-done
