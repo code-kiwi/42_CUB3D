fdb8051ab45db466170d9220e2dcf86572e9569d
Benjamin <benjamin.rappo69@gmail.com>
2024-06-18 11:28:12 +0200

    added a delta in the game_loop

--- srcs/game/game_loop.c
+++ srcs/game/game_loop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game_loop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codekiwi <codekiwi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:50:52 by mhotting          #+#    #+#             */
-/*   Updated: 2024/06/17 11:32:51 by codekiwi         ###   ########.fr       */
+/*   Updated: 2024/06/18 11:27:46 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,25 @@
 #include "mlx_api.h"
 #include "libft.h"
 
+#include <stdio.h>
+# include <sys/time.h>
+
 /**
  * @brief The main function called at each game loop iteration
  * @param game The project's game
  * @return A dummy integer
 */
+
+struct timeval GetTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv;
+}
+
 int	game_loop(t_game *game)
 {
+	suseconds_t	current_time;
+
 	if (game == NULL)
 		error_exit(game, ERR_GAME_LOOP);
 	game->mlx.event_loop_counter++;
@@ -31,7 +43,9 @@ int	game_loop(t_game *game)
 		update_player(&game->player, &game->map);
 		if (!cast_rays(&game->player, &game->map, game->rays))
 			error_exit(game, ERR_CAST_RAYS);
+		current_time = GetTimeStamp().tv_usec;
 		draw_walls(game);
+		printf ("delta : %ld\n", GetTimeStamp().tv_usec - current_time);
 		if (!t_mlx_render(&game->mlx))
 			error_exit(game, ERR_RENDER);
 		game->mlx.event_loop_counter = 0;
