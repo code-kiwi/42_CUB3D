bca1b03515b1b092c5b05fb8cfe42a610dabf2d9
Benjamin Rappo <brappo@z2r7p6.42lyon.fr>
2024-06-19 14:26:02 +0200

    cleaning

--- bonus/srcs_bonus/basic_render_bonus/draw_ceiling_ground.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_ceiling_ground.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/19 12:55:51 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/19 13:55:34by brappo           ###   ########.fr       */
+/*   Created: 2024/06/19 14:24:21 by brappo            #+#    #+#             */
+/*   Updated: 2024/06/19 14:25:10 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 #include <stdio.h>
 
-static	void	get_pixel_position_in_tile(t_mlx_coords *coords, t_ray *ray, 
+static	void	get_pixel_position_in_tile(t_mlx_coords *coords, t_ray *ray,
 	t_vector *player_position, t_vector *result)
 {
 	float		straight_line_dist;
@@ -25,31 +25,35 @@ static	void	get_pixel_position_in_tile(t_mlx_coords *coords, t_ray *ray,
 	double		temp;
 
 	straight_line_dist = (float)(WIN_HEIGHT / 2) / (coords->y - WIN_HEIGHT / 2);
-	pixel_dist = straight_line_dist / cos(ray->angle_from_orientation);
+	pixel_dist = straight_line_dist / ray->cos_angle_from_orientation;
 	result->x = player_position->x + ray->slope.x * pixel_dist;
 	result->y = player_position->y - ray->slope.y * pixel_dist;
 	result->x = modf(result->x, &temp);
 	result->y = modf(result->y, &temp);
 }
 
-static void	draw_pixel_ground(t_mlx_coords *coords, t_game *game, t_ray *ray)
+static void	draw_pixel_from_texture(t_vector *pos_in_tile,
+	t_mlx_coords *screen_coords, t_image *texture, t_image *screen)
 {
-	t_vector		pixel_pos;
-	char			*color;
 	t_mlx_coords	color_coords;
+	char			*color;
 
-	get_pixel_position_in_tile(coords, ray, &game->player.position, &pixel_pos);
-	color_coords.x = pixel_pos.x * game->textures[4].width;
-	color_coords.y = pixel_pos.y * game->textures[3].width;
-	color = t_mlx_get_pixel(&game->textures[4], color_coords.x, color_coords.y);
-	t_mlx_draw_pixel(game->mlx.img_buff, coords, *(unsigned int *)color);
+	color_coords.x = pos_in_tile->x * texture->width;
+	color_coords.y = pos_in_tile->y * texture->height;
+	color = t_mlx_get_pixel(texture, color_coords.x, color_coords.y);
+	t_mlx_draw_pixel(screen, screen_coords, *(unsigned int *)color);
 }
 
 void	draw_ground(t_mlx_coords *coords, int end, t_game *game, t_ray *ray)
 {
+	t_vector	pixel_pos;
+
 	while (coords->y < end)
 	{
-		draw_pixel_ground(coords, game, ray);
+		get_pixel_position_in_tile(coords, ray, &game->player.position,
+			&pixel_pos);
+		draw_pixel_from_texture(&pixel_pos, coords, &game->textures[4],
+			game->mlx.img_buff);
 		coords->y++;
 	}
 }
