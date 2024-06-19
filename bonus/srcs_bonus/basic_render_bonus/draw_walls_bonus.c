34981a42fee051061ab4174b0976a3be00b030fc
Benjamin Rappo <brappo@z2r7p6.42lyon.fr>
2024-06-19 15:08:36 +0200

    removed an unecessary call to draw_color_column

--- bonus/srcs_bonus/basic_render_bonus/draw_walls_bonus.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_walls_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 13:48:08 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/19 14:37:33 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/19 15:08:04 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ static void	draw_wall_column(size_t column_index, t_ray *ray, t_game *game)
 	t_image		*screen;
 
 	screen = game->mlx.img_buff;
-	column.coords.y = 0;
 	column.coords.x = column_index;
 	column.perceived_height = WIN_HEIGHT
 		/ (ray->length * cos(ray->angle_from_orientation));
@@ -49,7 +48,7 @@ static void	draw_wall_column(size_t column_index, t_ray *ray, t_game *game)
 	wall_end = floorf((WIN_HEIGHT + column.perceived_height) / 2);
 	if (wall_end > WIN_HEIGHT)
 		wall_end = WIN_HEIGHT;
-	draw_color_column(screen, &column.coords, 0xfefefe, column.wall_start);
+	column.coords.y = column.wall_start;
 	column.texture_start = column.coords.y - column.wall_start;
 	draw_texture_column(game->mlx.img_buff, &column, wall_end,
 		get_texture(game->textures, ray));
