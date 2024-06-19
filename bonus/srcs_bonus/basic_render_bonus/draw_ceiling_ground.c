47cf85a3f4401255b4534e5c7d05bf355afd2a93
Benjamin Rappo <brappo@z2r7p6.42lyon.fr>
2024-06-19 14:13:35 +0200

    cleaning : created a fonction get_pixel_position_in_tile

--- bonus/srcs_bonus/basic_render_bonus/draw_ceiling_ground.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_ceiling_ground.c
@@ -17,21 +17,30 @@
 
 #include <stdio.h>
 
-static void	draw_pixel_ground(t_mlx_coords *coords, t_game *game, t_ray *ray)
+static	void	get_pixel_position_in_tile(t_mlx_coords *coords, t_ray *ray, 
+	t_vector *player_position, t_vector *result)
 {
-	float			straight_line_dist;
-	float			pixel_dist;
-	t_vector		pixel_pos;
-	char			*color;
-	t_mlx_coords	color_coords;
-	double			temp;
+	float		straight_line_dist;
+	float		pixel_dist;
+	double		temp;
 
 	straight_line_dist = (float)(WIN_HEIGHT / 2) / (coords->y - WIN_HEIGHT / 2);
 	pixel_dist = straight_line_dist / cos(ray->angle_from_orientation);
-	pixel_pos.x = game->player.position.x + ray->slope.x * pixel_dist;
-	pixel_pos.y = game->player.position.y - ray->slope.y * pixel_dist;
-	color_coords.x = modf(pixel_pos.x, &temp) * game->textures[4].width;
-	color_coords.y = modf(pixel_pos.y, &temp) * game->textures[4].height;
+	result->x = player_position->x + ray->slope.x * pixel_dist;
+	result->y = player_position->y - ray->slope.y * pixel_dist;
+	result->x = modf(result->x, &temp);
+	result->y = modf(result->y, &temp);
+}
+
+static void	draw_pixel_ground(t_mlx_coords *coords, t_game *game, t_ray *ray)
+{
+	t_vector		pixel_pos;
+	char			*color;
+	t_mlx_coords	color_coords;
+
+	get_pixel_position_in_tile(coords, ray, &game->player.position, &pixel_pos);
+	color_coords.x = pixel_pos.x * game->textures[4].width;
+	color_coords.y = pixel_pos.y * game->textures[3].width;
 	color = t_mlx_get_pixel(&game->textures[4], color_coords.x, color_coords.y);
 	t_mlx_draw_pixel(game->mlx.img_buff, coords, *(unsigned int *)color);
 }
