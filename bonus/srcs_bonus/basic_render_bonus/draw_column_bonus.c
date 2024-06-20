541e21bb0f245eff04233bb661c76e87fb19f718
Benjamin Rappo <brappo@z4r12p3.42lyon.fr>
2024-06-20 12:12:21 +0200

    added invisible pixel

--- bonus/srcs_bonus/basic_render_bonus/draw_column_bonus.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_column_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 15:23:55 by root              #+#    #+#             */
-/*   Updated: 2024/06/20 11:22:33 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/20 12:10:47 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@
 
 void	draw_texture_column(t_image *screen, t_column *column, t_image *texture)
 {
-	char	*color;
-	float	scale_y;
-	char	*addr;
-	float	texture_pos;
+	char			*color_addr;
+	float			scale_y;
+	char			*addr;
+	float			texture_pos;
+	unsigned int	color;
 
 	scale_y = texture->height / column->perceived_height;
 	addr = screen->addr + (column->coords.y * screen->line_len \
 		+ column->coords.x * screen->bpp / 8);
-	color = texture->addr + column->texture_column * (screen->bpp / 8);
+	color_addr = texture->addr + column->texture_column * (screen->bpp / 8);
 	texture_pos = column->texture_start * scale_y;
 	while (column->coords.y < column->end)
 	{
-		*(unsigned int *)addr = *(unsigned int *)(color + \
+		color = *(unsigned int *)(color_addr + \
 			(int)texture_pos * texture->line_len);
+		if (color != 0xFF000000)
+			*(unsigned int *)addr = color;
 		addr += screen->line_len;
 		column->coords.y++;
 		texture_pos += scale_y;
