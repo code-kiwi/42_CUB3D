0940e391a25a785f17e23eef464b8ef3c3e5ddfd
mhotting <madric.hottinger.101@gmail.com>
2024-06-21 15:04:42 +0200

    Adds image sync for pause - Adds pause boolean and key handler for pause

--- bonus/srcs_bonus/mlx_api_bonus/t_mlx_sync_images.c
+++ bonus/srcs_bonus/mlx_api_bonus/t_mlx_sync_images.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 12:07:43 by mhotting          #+#    #+#             */
-/*   Updated: 2024/06/21 14:07:06 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/06/21 15:04:38 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 void	t_mlx_sync_images(t_mlx *mlx)
 {
-	unsigned int	*dest_addr;
-	unsigned int	*buff_addr;
+	char	*dest_addr;
+	char	*buff_addr;
 	int		i;
 	int		j; 
 
 	if (mlx == NULL)
 		return ;
-	buff_addr = (unsigned int *) mlx->img_buff->addr;
+	buff_addr = mlx->img_buff->addr;
 	if (mlx->img_buff == mlx->img1)
-		dest_addr = (unsigned int *) mlx->img2->addr;
+		dest_addr = mlx->img2->addr;
 	else
-		dest_addr = (unsigned int *) mlx->img1->addr;
+		dest_addr = mlx->img1->addr;
 	i = 0;
 	while (i < mlx->img_buff->height)
 	{
-		buff_addr += mlx->img_buff->line_len / 4;
-		dest_addr += mlx->img_buff->line_len / 4;
+		buff_addr += mlx->img_buff->line_len;
+		dest_addr += mlx->img_buff->line_len;
 		j = 0;
 		while (j < mlx->img_buff->width)
 		{
