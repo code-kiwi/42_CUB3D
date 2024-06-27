9f8df5b52b290a351bbd525aa62811a3b5887038
Benjamin <benjamin.rappo69@gmail.com>
2024-06-27 12:27:05 +0200

    divided the add_path_node to multiples functions, not working

--- bonus/srcs_bonus/pathfinding_bonus/add_path_node_bonus.c
+++ bonus/srcs_bonus/pathfinding_bonus/add_path_node_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 10:30:00 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/27 12:07:55 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:19:26 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,46 +47,52 @@ t_stack_path	*get_node(t_mlx_coords *position, t_stack_path *stack)
 	return (NULL);
 }
 
+void	update_existing_node(t_stack_path *new_node, size_t total_cost)
+{
+	if (new_node->total_cost < total_cost)
+		return ;
+	if (new_node->previous != NULL)
+		new_node->previous->next = new_node->next;
+	if (new_node->next != NULL)
+		new_node->next->previous = new_node->previous;
+}
+
+t_stack_path	*add_new_node(t_mlx_coords *position)
+{
+	t_stack_path	*new_node;
+
+	new_node = malloc(sizeof(t_stack_path));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->position.x = position->x;
+	new_node->position.y = position->y;
+	return (new_node);
+}
+
 bool	add_path_node(t_mlx_coords *position, t_pathfinding *pathfinding,
 	t_stack_path *previous, size_t distance)
 {
 	t_stack_path	*new_node;
 	size_t			start_distance;
 	size_t			end_distance;
-	size_t			total_cost;
 
 	end_distance = get_end_distance(position, pathfinding->end);
 	if (previous != NULL)
 		start_distance = previous->start_distance + distance;
 	else
 		start_distance = 0;
-	total_cost = end_distance + start_distance;
 	new_node = get_node(position, pathfinding->stack);
-	if (new_node != NULL)
-	{
-		if (new_node->total_cost < total_cost)
-			return (true);
-		if (new_node->previous != NULL)
-			new_node->previous->next = new_node->next;
-		if (new_node->next != NULL)
-			new_node->next->previous = new_node->previous;
-		new_node->previous_tile = previous;
-		new_node->end_distance = end_distance;
-		new_node->start_distance = start_distance;
-		new_node->total_cost = total_cost;
-		insert_path_node(&pathfinding->stack, new_node);
-		return (true);
-	}
-	new_node = malloc(sizeof(t_stack_path));
+	if (new_node == NULL)
+		new_node = add_new_node(position);
+	else
+		update_existing_node(new_node, start_distance + end_distance);
 	if (new_node == NULL)
 		return (false);
-	new_node->position.x = position->x;
-	new_node->position.y = position->y;
 	new_node->previous_tile = previous;
 	new_node->next = NULL;
 	new_node->start_distance = start_distance;
 	new_node->end_distance = end_distance;
-	new_node->total_cost = total_cost;
+	new_node->total_cost = start_distance + end_distance;
 	insert_path_node(&pathfinding->stack, new_node);
 	return (true);
 }
