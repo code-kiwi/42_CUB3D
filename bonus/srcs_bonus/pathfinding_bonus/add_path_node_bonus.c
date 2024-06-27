5af654f0552e270fa21af577adf442454d2cce9d
Benjamin <benjamin.rappo69@gmail.com>
2024-06-27 12:43:41 +0200

    norminette OK, program working

--- bonus/srcs_bonus/pathfinding_bonus/add_path_node_bonus.c
+++ bonus/srcs_bonus/pathfinding_bonus/add_path_node_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 10:30:00 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/27 12:19:26 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:42:12 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,15 @@ t_stack_path	*get_node(t_mlx_coords *position, t_stack_path *stack)
 	return (NULL);
 }
 
-void	update_existing_node(t_stack_path *new_node, size_t total_cost)
+static bool	update_existing_node(t_stack_path *new_node, size_t total_cost)
 {
 	if (new_node->total_cost < total_cost)
-		return ;
+		return (true);
 	if (new_node->previous != NULL)
 		new_node->previous->next = new_node->next;
 	if (new_node->next != NULL)
 		new_node->next->previous = new_node->previous;
+	return (false);
 }
 
 t_stack_path	*add_new_node(t_mlx_coords *position)
@@ -84,8 +85,8 @@ bool	add_path_node(t_mlx_coords *position, t_pathfinding *pathfinding,
 	new_node = get_node(position, pathfinding->stack);
 	if (new_node == NULL)
 		new_node = add_new_node(position);
-	else
-		update_existing_node(new_node, start_distance + end_distance);
+	else if (update_existing_node(new_node, start_distance + end_distance))
+		return (true);
 	if (new_node == NULL)
 		return (false);
 	new_node->previous_tile = previous;
--- bonus/srcs_bonus/pathfinding_bonus/pathfinding_bonus.c
+++ bonus/srcs_bonus/pathfinding_bonus/pathfinding_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 19:09:13 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/27 12:07:19 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/27 12:43:00 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,6 @@
 #include "libft.h"
 
 #include <unistd.h>
-
-#include <stdio.h>
-
-void	print_stack(t_stack_path *stack, size_t max_index)
-{
-	size_t	index;
-
-	index = 0;
-	printf("\n");
-	while (index < max_index && stack)
-	{
-		printf("%ld, Gcost : %ld, Hcost : %ld, Fcost : %ld, pos : (%d,%d)\n", index, stack->start_distance, stack->end_distance, stack->total_cost, stack->position.x, stack->position.y);
-		stack = stack->next;
-		index++;
-	}
-}
 
 t_list	*find_path(t_mlx_coords *start, t_mlx_coords *end, t_map *map)
 {
@@ -41,11 +25,8 @@ t_list	*find_path(t_mlx_coords *start, t_mlx_coords *end, t_map *map)
 	while (true)
 	{
 		lock_tile(&pathfinding);
-
 		if (!add_neighboring_tiles(&pathfinding, map))
 			return (t_pathfinding_free(&pathfinding), NULL);
-		// print_stack(pathfinding.stack, 10);
-		// sleep(1);
 		if (pathfinding.stack->position.x == end->x
 			&& pathfinding.stack->position.y == end->y)
 			break ;
