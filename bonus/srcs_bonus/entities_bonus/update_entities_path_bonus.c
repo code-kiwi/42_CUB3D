b100c23bd712e4a6f78663dd00d44692776088dc
Benjamin <benjamin.rappo69@gmail.com>
2024-06-29 15:35:41 +0200

    the entity path is update to follow the player when the entity can see him

--- bonus/srcs_bonus/entities_bonus/update_entities_path_bonus.c
+++ bonus/srcs_bonus/entities_bonus/update_entities_path_bonus.c
@@ -6,14 +6,15 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 15:05:48 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/29 15:28:40 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/29 15:33:57 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "entities_bonus.h"
 #include "cub3d_bonus.h"
+#include "pathfinding_bonus.h"
 
-bool	can_see(t_vector *position, t_vector *point, t_game *game)
+static bool	can_see(t_vector *position, t_vector *point, t_game *game)
 {
 	t_ray		ray;
 	float		distance;
@@ -28,6 +29,19 @@ bool	can_see(t_vector *position, t_vector *point, t_game *game)
 	return (ray_length >= distance);
 }
 
+static void	update_path(t_entity *entity, t_game *game)
+{
+	t_mlx_coords	start;
+	t_mlx_coords	end;
+
+	ft_lstclear(&entity->path, free);
+	start.x = entity->sprite->position.x;
+	start.y = entity->sprite->position.y;
+	end.x = game->player.position.x;
+	end.y = game->player.position.y;
+	entity->path = find_path(&start, &end, &game->map);
+}
+
 bool	update_entities_path(t_game *game)
 {
 	t_list		*current;
@@ -39,6 +53,8 @@ bool	update_entities_path(t_game *game)
 	while (current)
 	{
 		entity = current->content;
+		if (can_see(&entity->sprite->position, &game->player.position, game))
+			update_path(entity, game);
 		current = current->next;
 	}
 	return (true);
