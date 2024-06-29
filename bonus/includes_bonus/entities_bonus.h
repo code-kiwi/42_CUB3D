f5e15aac9612a70cdf788708a902bc321d31b38c
Benjamin <benjamin.rappo69@gmail.com>
2024-06-29 14:07:10 +0200

    the sprite in entity is now stacked

--- bonus/includes_bonus/entities_bonus.h
+++ bonus/includes_bonus/entities_bonus.h
@@ -6,17 +6,18 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 10:07:32 by brappo            #+#    #+#             */
-/*   Updated: 2024/06/29 12:39:27 by brappo           ###   ########.fr       */
+/*   Updated: 2024/06/29 14:06:19 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENTITIES_BONUS_H
 # define ENTITIES_BONUS_H
 
+# include "sprite_bonus.h"
+
 # include <stdbool.h>
 
 typedef struct s_entity	t_entity;
-typedef struct s_sprite	t_sprite;
 typedef struct s_list	t_list;
 
 struct s_entity
@@ -24,7 +25,7 @@ struct s_entity
 	float		speed;
 	bool		is_path_circular;
 	t_list		*path;
-	t_sprite	*sprite;
+	t_sprite	sprite;
 };
 
 void	update_entities(t_list *entities, float delta_time);
