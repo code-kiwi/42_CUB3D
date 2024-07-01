7e82d00cb591e93a22899f901c0be6f2e340384c
Benjamin <benjamin.rappo69@gmail.com>
2024-07-01 15:20:26 +0200

    added a use_gun function

--- bonus/includes_bonus/item_bonus.h
+++ bonus/includes_bonus/item_bonus.h
@@ -6,20 +6,24 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:33 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/01 10:10:37 by brappo           ###   ########.fr       */
+/*   Updated: 2024/07/01 11:44:27 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITEM_BONUS_H
 # define ITEM_BONUS_H
 
+#include <sys/types.h>
+
 typedef struct s_image	t_image;
 typedef struct s_item	t_item;
+typedef struct s_game	t_game;
 
 struct s_item
 {
-	void	(*use_item)(void *);
+	void	(*use_item)(t_game *);
 	t_image	*texture;
+	ssize_t	remaining_usage;
 };
 
 #endif // !ITEM_BONUS_H
--- bonus/srcs_bonus/item_bonus/item_bonus.c
+++ bonus/srcs_bonus/item_bonus/item_bonus.c
@@ -6,8 +6,41 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 10:03:22 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/01 10:03:27 by brappo           ###   ########.fr       */
+/*   Updated: 2024/07/01 15:18:52 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "item_bonus.h"
+#include "cub3d_bonus.h"
+#include "entities_bonus.h"
+
+int	is_sprite_of_entity(void *entity_void, void *sprite_void)
+{
+	t_entity	*entity;
+	t_sprite	*sprite;
+
+	entity = entity_void;
+	sprite = sprite_void;
+
+	return (!(entity->sprite == sprite));
+}
+
+int	equal(void *a, void *b)
+{
+	return (!(a == b));
+}
+
+void	use_gun(t_game *game)
+{
+	t_list	*aimed_entity;
+
+	aimed_entity = ft_lstfind(game->entities, game->player.aimed_sprite,
+		is_sprite_of_entity);
+	if (aimed_entity != NULL)
+	{
+		ft_lst_remove_if(&game->entities, aimed_entity->content, equal,
+			t_entity_destroy);
+	}
+	ft_lst_remove_if(&game->sprites, game->player.aimed_sprite, equal, free);
+	game->player.aimed_sprite = NULL;
+}
