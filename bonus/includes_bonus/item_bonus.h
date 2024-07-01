e8ec6e86784b5dea0771fcae18e190ba49dc2443
Benjamin <benjamin.rappo69@gmail.com>
2024-07-01 10:10:53 +0200

    norminette OK

--- bonus/includes_bonus/item_bonus.h
+++ bonus/includes_bonus/item_bonus.h
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:33 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/01 10:05:46 by brappo           ###   ########.fr       */
+/*   Updated: 2024/07/01 10:10:37 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 # define ITEM_BONUS_H
 
 typedef struct s_image	t_image;
+typedef struct s_item	t_item;
 
-struct t_item
+struct s_item
 {
 	void	(*use_item)(void *);
 	t_image	*texture;
 };
-
 
 #endif // !ITEM_BONUS_H
