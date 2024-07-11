b6cc7d4889db019a5e40b2a2b72506b095ebc37e
root <root@BenjaminMaria>
2024-07-11 11:05:29 +0200

    fixed segfault when killing the last_entity_updated

--- bonus/srcs_bonus/entities_bonus/damage_entity_bonus.c
+++ bonus/srcs_bonus/entities_bonus/damage_entity_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   damage_entity_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 18:53:40 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/01 19:07:54 by brappo           ###   ########.fr       */
+/*   Updated: 2024/07/11 11:03:03 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ static void	kill_entity(t_game *game, t_entity *entity)
 {
 	if (game == NULL || entity == NULL || game->entities == NULL)
 		return ;
+	if (game->last_entity_updated->content == entity)
+		game->last_entity_updated = game->last_entity_updated->next;
 	ft_lst_remove_if(&game->entities, entity, equal, t_entity_destroy);
 	ft_lst_remove_if(&game->sprites, game->player.aimed_sprite, equal, free);
 }
