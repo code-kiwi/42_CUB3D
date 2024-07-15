8d010c83c7c9729b44f615d99ca7bc16f4f8d510
mhotting <madric.hottinger.101@gmail.com>
2024-07-15 09:06:41 +0200

    Norme applied to draw_circle

--- bonus/srcs_bonus/mlx_api_bonus/t_mlx_draw_circle_bonus.c
+++ bonus/srcs_bonus/mlx_api_bonus/t_mlx_draw_circle_bonus.c
@@ -3,14 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   t_mlx_draw_circle_bonus.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codekiwi <codekiwi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 22:09:10 by codekiwi          #+#    #+#             */
-/*   Updated: 2024/07/14 23:43:11 by codekiwi         ###   ########.fr       */
+/*   Updated: 2024/07/15 09:05:58 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx_api_bonus.h"
+
+static void	update_err(t_mlx_coords *xy, int *err)
+{
+	if (*err <= 0)
+	{
+		xy->y += 1;
+		*err += 2 * xy->y + 1;
+	}
+	if (*err > 0)
+	{
+		xy->x -= 1;
+		*err -= 2 * xy->x + 1;
+	}
+}
+
+static void	init_vars(t_mlx_coords *xy, int *err, int radius)
+{
+	xy->x = radius;
+	xy->y = 0;
+	*err = 0;
+}
+
+static void	update_temp(t_mlx_coords *temp, int x, int y)
+{
+	temp->x = x;
+	temp->y = y;
+}
 
 void	t_mlx_draw_circle(
 	t_image *img,
@@ -19,49 +46,29 @@ void	t_mlx_draw_circle(
 	uint32_t color
 )
 {
-	int				x;
-	int				y;
+	t_mlx_coords	xy;
 	int				err;
 	t_mlx_coords	temp;
 
-	x = radius;
-	y = 0;
-	err = 0;
-	while (x >= y)
+	init_vars(&xy, &err, radius);
+	while (xy.x >= xy.y)
 	{
-		temp.x = coords->x + x;
-		temp.y = coords->y + y;
+		update_temp(&temp, coords->x + xy.x, coords->y + xy.y);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x + y;
-		temp.y = coords->y + x;
+		update_temp(&temp, coords->x + xy.y, coords->y + xy.x);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x - y;
-		temp.y = coords->y + x;
+		update_temp(&temp, coords->x - xy.y, coords->y + xy.x);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x - x;
-		temp.y = coords->y + y;
+		update_temp(&temp, coords->x - xy.x, coords->y + xy.y);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x - x;
-		temp.y = coords->y - y;
+		update_temp(&temp, coords->x - xy.x, coords->y - xy.y);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x - y;
-		temp.y = coords->y - x;
+		update_temp(&temp, coords->x - xy.y, coords->y - xy.x);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x + y;
-		temp.y = coords->y - x;
+		update_temp(&temp, coords->x + xy.y, coords->y - xy.x);
 		t_mlx_draw_pixel(img, &temp, color);
-		temp.x = coords->x + x;
-		temp.y = coords->y - y;
+		update_temp(&temp, coords->x + xy.x, coords->y - xy.y);
 		t_mlx_draw_pixel(img, &temp, color);
-		if (err <= 0)
-		{
-			y += 1;
-			err += 2 * y + 1;
-		}
-		if (err > 0)
-		{
-			x -= 1;
-			err -= 2 * x + 1;
-		}
+		update_err(&xy, &err);
 	}
 }
