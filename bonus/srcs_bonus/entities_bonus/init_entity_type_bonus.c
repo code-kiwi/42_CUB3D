27ea83f68a55c36a64c68eded03b96b5d079d26e
root <root@BenjaminMaria>
2024-07-17 09:48:24 +0200

    norminette OK, program compile

--- bonus/srcs_bonus/entities_bonus/init_entity_type_bonus.c
+++ bonus/srcs_bonus/entities_bonus/init_entity_type_bonus.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   init_entity_type_bonus.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 10:26:19 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/16 11:55:56 by brappo           ###   ########.fr       */
+/*   Updated: 2024/07/17 09:46:53 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 #include "entities_bonus.h"
 
-bool	init_entity_type(t_entity *entity, char id, t_list *textures[12])
+bool	init_entity_type(t_entity *entity, char id, \
+	t_list *textures[MAP_NB_IDS])
 {
 	if (id == ID_MAP_DEMON)
 		return (demon_init(entity, textures));
