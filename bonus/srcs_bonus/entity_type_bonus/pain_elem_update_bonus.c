8c3381b2d67a9edcfaebca41faacdcf78e27a100
Benjamin Rappo <brappo@z3r2p3.42lyon.fr>
2024-07-22 14:00:20 +0200

    removed the temporary code

--- bonus/srcs_bonus/entity_type_bonus/pain_elem_update_bonus.c
+++ bonus/srcs_bonus/entity_type_bonus/pain_elem_update_bonus.c
@@ -26,24 +26,15 @@ static void	pain_elem_close_attack(t_entity *entity, t_sprite *sprite, t_game *g
 
 static bool	pain_elem_spawn(t_entity *entity, t_sprite *sprite, t_game *game)
 {
-	t_vector	direction;
-	t_vector	position;
-	float		radius;
+	t_vector	*position;
 
 	if (entity->cooldown > 0)
 		return (true);
-	direction.x = game->player.position.x - entity->sprite->position.x;
-	direction.y = game->player.position.y - entity->sprite->position.y;
-	normalize_vector(&direction);
-	radius = sqrt(entity->squared_radius) + 0.1f;
-	position.x = entity->sprite->position.x + direction.x * radius;
-	position.y = entity->sprite->position.y + direction.y * radius;
+	position = &sprite->position;
 	sprite->next_animation = game->textures[IDX_TXTR_PAIN_ELEM_WALK];
 	sprite->animation = game->textures[IDX_TXTR_PAIN_ELEM_ATTACK];
-	if (!is_walkable(&game->map, )
-		|| collide_entity(game->entities, &position))
 	entity->cooldown = PAIN_ELEM_SPAW_PAUSE;
-	if (!add_entity(game, position.x, position.y, ID_MAP_LOST_SOUL))
+	if (!add_entity(game, position->x, position->y, ID_MAP_LOST_SOUL))
 		return (false);
 	return (true);
 }
