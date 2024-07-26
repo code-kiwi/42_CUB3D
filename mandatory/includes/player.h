0994acd9e4e5990db010a56c1d65519a6e0bff11
root <root@BenjaminMaria>
2024-07-26 21:15:27 +0200

    added defines for the y rotation speed

--- mandatory/includes/player.h
+++ mandatory/includes/player.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 12:04:34 by mhotting          #+#    #+#             */
-/*   Updated: 2024/07/26 20:51:54 by root             ###   ########.fr       */
+/*   Updated: 2024/07/26 21:14:22 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # define LEFT 				1
 # define BACK 				2
 # define RIGHT 				3
+# define PLAYER_Y_ROTATION	300
 
 typedef struct s_player	t_player;
 
--- mandatory/srcs/basic_render/draw_walls.c
+++ mandatory/srcs/basic_render/draw_walls.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 13:48:08 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/26 21:10:46 by root             ###   ########.fr       */
+/*   Updated: 2024/07/26 21:14:33 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,8 @@ static void	draw_wall_column(size_t column_index, t_ray *ray, t_game *game)
 	column.ray = ray;
 	wall_start = floorf((WIN_HEIGHT - column.perceived_height) / 2);
 	wall_end = floorf((WIN_HEIGHT + column.perceived_height) / 2);
-	wall_start -= game->player.orientation.y * 300;
-	wall_end -= game->player.orientation.y * 300;
+	wall_start -= game->player.orientation.y * PLAYER_Y_ROTATION;
+	wall_end -= game->player.orientation.y * PLAYER_Y_ROTATION;
 	if (wall_end > WIN_HEIGHT)
 		wall_end = WIN_HEIGHT;
 	draw_color_column(screen, &column.coords, game->ground_color, wall_start);
