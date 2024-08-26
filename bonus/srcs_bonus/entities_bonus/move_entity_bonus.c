dd7b1dd4c99090f05e18a2ef5c3de08009bad725
mhotting <madric.hottinger.101@gmail.com>
2024-08-26 17:04:14 +0200

    Changes collision handling

--- bonus/srcs_bonus/entities_bonus/move_entity_bonus.c
+++ bonus/srcs_bonus/entities_bonus/move_entity_bonus.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   move_entity_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 17:22:48 by brappo            #+#    #+#             */
-/*   Updated: 2024/07/22 14:09:18 by brappo           ###   ########.fr       */
+/*   Updated: 2024/08/26 17:02:36 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <math.h>
 
 #include "entities_bonus.h"
 #include "mlx_api_bonus.h"
@@ -45,8 +47,8 @@ bool	move_entity(t_list *entities, t_vector *position, t_vector *move,
 	save.x = position->x;
 	save.y = position->y;
 	add_vector(position, move);
-	tile_pos.x = position->x;
-	tile_pos.y = position->y;
+	tile_pos.x = position->x + 0.1 * (fabs(move->x) < 0.001f ? 0.0f : (move->x < 0.0f ? -1 : 1));
+	tile_pos.y = position->y + 0.1 * (fabs(move->y) < 0.001f ? 0.0f : (move->y < 0.0f ? -1 : 1));
 	if (!is_walkable(map, &tile_pos)
 		|| collide_entity(entities, position))
 	{
