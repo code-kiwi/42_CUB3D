9af97af787f659f08d49468c3596b4e0ff908fa1
Benjamin Rappo <brappo@z3r2p1.42lyon.fr>
2024-09-03 09:30:48 +0200

    replace the select previous wepon and select next weapon by select weapon with an index

--- bonus/srcs_bonus/player_bonus/weapons_utils_bonus.c
+++ bonus/srcs_bonus/player_bonus/weapons_utils_bonus.c
@@ -3,48 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   weapons_utils_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 13:04:12 by mhotting          #+#    #+#             */
-/*   Updated: 2024/08/31 11:43:08 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/09/03 09:29:24 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 #include "libft.h"
 
-void	player_select_prev_weapon(t_player_weapon *weapon_info)
+void	player_select_weapon(t_player_weapon *weapon_info, int index)
 {
 	if (
 		weapon_info == NULL
 		|| weapon_info->weapon_state != WEAPON_STATE_IDLE
 	)
 		return ;
+	if (index == NB_TOT_WEAPONS)
+		 index = 0;
+	if (index == -1)
+		index = NB_TOT_WEAPONS - 1;
 	weapon_info->weapon_state = WEAPON_STATE_HOLSTERING;
 	weapon_info->frame_update_delta = 0;
-	weapon_info->next_weapon_index = weapon_info->curr_weapon_index;
-	if (weapon_info->next_weapon_index == 0)
-	{
-		while (weapon_info->weapons[weapon_info->next_weapon_index + 1])
-			weapon_info->next_weapon_index++;
-	}
-	else
-		weapon_info->next_weapon_index--;
-}
-
-void	player_select_next_weapon(t_player_weapon *weapon_info)
-{
-	if (
-		weapon_info == NULL
-		|| weapon_info->weapon_state != WEAPON_STATE_IDLE
-	)
-		return ;
-	weapon_info->weapon_state = WEAPON_STATE_HOLSTERING;
-	weapon_info->frame_update_delta = 0;
-	weapon_info->next_weapon_index = weapon_info->curr_weapon_index;
-	weapon_info->next_weapon_index++;
-	if (weapon_info->weapons[weapon_info->next_weapon_index] == NULL)
-		weapon_info->next_weapon_index = 0;
+	weapon_info->next_weapon_index = index;
 }
 
 void	player_weapon_use(t_player_weapon *weapon_info, t_game *game)
