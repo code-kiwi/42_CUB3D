7fe0961d2673887b59a23bd7edf8aa2dc1356450
Benjamin <benjamin.rappo69@gmail.com>
2024-09-04 08:07:06 +0200

    changed the name from offset to camera_offset

--- bonus/srcs_bonus/basic_render_bonus/draw_walls_bonus.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_walls_bonus.c
@@ -67,19 +67,19 @@ static void	draw_wall_column(size_t column_index, t_ray *ray, t_game *game)
 {
 	t_column	column;
 	t_image		*texture;
-	int			offset;
+	int			camera_offset;
 	int			player_height_offset;
 	int			diff;
 	int			saveEnd;
 
-	offset = game->player.orientation.y;
+	camera_offset = game->player.orientation.y;
 	column.coords.x = column_index;
 	column.perceived_height = WIN_HEIGHT
 		/ (ray->length * ray->cos_angle_from_orientation);
 	player_height_offset = get_player_height_offset(column.perceived_height, game->player.camera_y_diff);
-	column.start = floorf((WIN_HEIGHT - column.perceived_height) / 2) + offset + player_height_offset;
-	column.end = range((WIN_HEIGHT + column.perceived_height) / 2 + offset + player_height_offset);
-	saveEnd = (WIN_HEIGHT + column.perceived_height) / 2 + offset + player_height_offset;
+	column.start = floorf((WIN_HEIGHT - column.perceived_height) / 2) + camera_offset + player_height_offset;
+	column.end = range((WIN_HEIGHT + column.perceived_height) / 2 + camera_offset + player_height_offset);
+	saveEnd = (WIN_HEIGHT + column.perceived_height) / 2 + camera_offset + player_height_offset;
 	column.coords.y = range(column.start);
 	column.texture_start = column.coords.y - column.start;
 	texture = get_texture(game->anim, ray);
