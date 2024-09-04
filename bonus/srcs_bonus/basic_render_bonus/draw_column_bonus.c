c1bc6336d59aa7dfc4e5bcf6c4c22fac52b86c71
Benjamin <benjamin.rappo69@gmail.com>
2024-09-04 14:21:14 +0200

    added comments to draw_texture_column and offset_bonus

--- bonus/srcs_bonus/basic_render_bonus/draw_column_bonus.c
+++ bonus/srcs_bonus/basic_render_bonus/draw_column_bonus.c
@@ -6,12 +6,17 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 15:23:55 by root              #+#    #+#             */
-/*   Updated: 2024/09/04 10:41:35 by brappo           ###   ########.fr       */
+/*   Updated: 2024/09/04 14:18:58 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
+/// @brief Draw a wall with a texture
+/// @param screen The screen to draw to.
+/// @param column The column data to draw
+/// @param texture The texture to apply to the wall
+/// @param distance THe distance of the wall from teh player
 void	draw_texture_column(t_image *screen, t_column *column, t_image *texture,
 	float distance)
 {
@@ -24,6 +29,7 @@ void	draw_texture_column(t_image *screen, t_column *column, t_image *texture,
 	scale_y = texture->height / column->perceived_height;
 	addr = screen->addr + (column->coords.y * screen->line_len \
 		+ column->coords.x * screen->bpp_factor);
+	// addr = t_mlx_get_pixel(screen, column->coords.x, column->coords.y);
 	color_addr = texture->addr + column->texture_x * screen->bpp_factor;
 	texture_pos = column->texture_start * scale_y;
 	while (column->coords.y < column->ranged_end)
