fa41ae365111cea258ce7aa3ba779c59d92db4f6
Benjamin Rappo <brappo@z3r2p1.42lyon.fr>
2024-09-05 13:58:39 +0200

    ca marche !!!!!!!!

--- bonus/srcs_bonus/render_bonus/draw_sky_bonus.c
+++ bonus/srcs_bonus/render_bonus/draw_sky_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:14:16 by brappo            #+#    #+#             */
-/*   Updated: 2024/09/05 13:30:37 by brappo           ###   ########.fr       */
+/*   Updated: 2024/09/05 13:58:09 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <math.h>
 
 void	draw_sky_column(t_image *screen, float angle, t_image *texture, \
-	int column_index, int diff)
+	int column_index, int offset)
 {
 	char			*addr;
 	char			*color_addr;
@@ -29,7 +29,7 @@ void	draw_sky_column(t_image *screen, float angle, t_image *texture, \
 	addr = t_mlx_get_pixel(screen, column_index, 0);
 	scale_y = (float)texture->height / 3 / WIN_HEIGHT;
 	color_addr = t_mlx_get_pixel(texture, angle * (float)texture->width / (PI * 2), texture->height / 3);
-	texture_pos = diff * scale_y;
+	texture_pos = offset * scale_y;
 	while (index < WIN_HEIGHT)
 	{
 		color = *(unsigned int *)(color_addr + \
@@ -47,18 +47,23 @@ void	draw_sky(t_game *game)
 	float	column_angle;
 	t_image	*screen;
 	t_image	*sky_texture;
+	t_ray	*ray;
+	int		offset;
 
 	index = 0;
 	screen = game->mlx.img_buff;
 	sky_texture = game->anim[IDX_TXTR_SKY].textures->content;
 	while (index < WIN_WIDTH)
 	{
-		column_angle = atan2f(-game->rays[index].slope.y, game->rays[index].slope.x);
+		ray = &game->rays[index];
+		column_angle = atan2f(-ray->slope.y, ray->slope.x);
 		if (column_angle < 0)
 			column_angle += 2 * PI;
 		else if (column_angle > 2 * PI)
 			column_angle -= 2 * PI;
-		draw_sky_column(screen, column_angle, sky_texture, index, -game->player.orientation.y);
+		offset = game->player.camera_y - game->player.height;
+		offset -= game->player.orientation.y;
+		draw_sky_column(screen, column_angle, sky_texture, index, offset);
 		index++;
 	}
 }
