61dc0a25329db6b4c328f72a3610eac4cd4f9faa
Benjamin Rappo <brappo@z3r2p1.42lyon.fr>
2024-09-05 14:30:18 +0200

    removed the draw_sky_texture_column and replaced it with the temp, divided this function

--- bonus/srcs_bonus/render_bonus/draw_sky_bonus.c
+++ bonus/srcs_bonus/render_bonus/draw_sky_bonus.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 11:14:16 by brappo            #+#    #+#             */
-/*   Updated: 2024/09/05 14:22:44 by brappo           ###   ########.fr       */
+/*   Updated: 2024/09/05 14:29:30 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,57 +15,49 @@
 
 #include <math.h>
 
-void	draw_sky_column(t_image *screen, float angle, t_image *texture, \
-	int column_index, int offset)
+static float	get_column_angle(t_ray *ray)
 {
-	char			*addr;
-	char			*color_addr;
-	int				index;
-	float			texture_pos;
-	float			scale_y;
-	unsigned int	color;
-
-	index = 0;
-	addr = t_mlx_get_pixel(screen, column_index, 0);
-	scale_y = (float)texture->height / 3 / WIN_HEIGHT;
-	color_addr = t_mlx_get_pixel(texture, angle * (float)texture->width / (PI * 2), texture->height / 3);
-	texture_pos = offset * scale_y;
-	while (index < WIN_HEIGHT)
-	{
-		color = *(unsigned int *)(color_addr + \
-			(int)texture_pos * texture->line_len);
-		*(unsigned int *)addr = color;
-		texture_pos += scale_y;
-		index++;
-		addr += screen->line_len;
-	}
-}
-
-void	draw_sky_column_temp(size_t column_index, t_ray *ray, t_game *game)
-{
-	int			offset;
-	float		column_angle;
-	t_column	column;
-	t_image		*screen;
-	t_image		*texture;
-	float		scale_y;
+	float	column_angle;
 
 	column_angle = atan2f(-ray->slope.y, ray->slope.x);
 	if (column_angle < 0)
 		column_angle += 2 * PI;
 	else if (column_angle > 2 * PI)
 		column_angle -= 2 * PI;
+	return (column_angle);
+}
+
+static size_t	get_texture_start(t_player *player, t_image *sky_texture, \
+	float perceived_height)
+{
+	size_t	texture_start;
+	int		offset;
+	float	scale_y;
+
+	offset = player->camera_y - player->height;
+	offset -= player->orientation.y;
+	scale_y = sky_texture->height / perceived_height;
+	texture_start = offset + sky_texture->height / 3 / scale_y;
+	return (texture_start);
+}
+
+static void	draw_sky_column(size_t column_index, t_ray *ray, t_game *game)
+{
+	float		column_angle;
+	t_column	column;
+	t_image		*screen;
+	t_image		*texture;
+
+	column_angle = get_column_angle(ray);
 	screen = game->mlx.img_buff;
 	texture = game->anim[IDX_TXTR_SKY].textures->content;
-	offset = game->player.camera_y - game->player.height;
-	offset -= game->player.orientation.y;
 	column.perceived_height = WIN_HEIGHT * 3;
 	column.coords.y = 0;
 	column.coords.x = column_index;
 	column.texture_x = column_angle * (float)texture->width / (PI * 2);
 	column.ranged_end = WIN_HEIGHT;
-	scale_y = texture->height / column.perceived_height;
-	column.texture_start = offset + texture->height / 3 / scale_y;
+	column.texture_start = get_texture_start(&game->player, texture, \
+		column.perceived_height);
 	draw_texture_column(screen, &column, texture, 0);
 }
 
@@ -80,7 +72,7 @@ void	draw_sky(t_game *game)
 	while (index < WIN_WIDTH)
 	{
 		ray = &game->rays[index];
-		draw_sky_column_temp(index, ray, game);
+		draw_sky_column(index, ray, game);
 		index++;
 	}
 }
